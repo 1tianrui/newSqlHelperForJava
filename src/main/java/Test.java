e015bab9e3e7ed70ccc5f983bfadabac28e2587d
tianrui <tianrui03@meituan.com>
2016-08-23 14:21:58 +0800

    fix

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -16,27 +16,7 @@ public class Test {
         ids.add("insert");
         ids.add("select");
         ids.add("update");
-        Sql2Spring test = new Sql2Spring("CREATE TABLE `YS_FIN_NewMonthlyBill` (\n" +
-                "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-                "  `shop_id` int(11) DEFAULT NULL COMMENT '商户编号',\n" +
-                "  `bill_year` int(11) DEFAULT NULL,\n" +
-                "  `bill_month` int(11) DEFAULT NULL,\n" +
-                "  `pay_rule_id` int(11) DEFAULT NULL COMMENT '收费模式编号',\n" +
-                "  `discount` int(11) DEFAULT NULL COMMENT '打折比例,0-100',\n" +
-                "  `origin_paid` decimal(10,2) DEFAULT NULL COMMENT '应扣',\n" +
-                "  `actual_paid` decimal(10,2) DEFAULT NULL COMMENT '实扣',\n" +
-                "  `discount_amount` decimal(10,2) DEFAULT NULL COMMENT '折扣金额',\n" +
-                "  `record_total_cnt` int(11) DEFAULT NULL COMMENT '当月成功订单数量',\n" +
-                "  `record_needpaid_cnt` int(11) DEFAULT NULL COMMENT '可扣费订单数量',\n" +
-                "  `record_onlinepay_cnt` int(11) DEFAULT NULL COMMENT '在线支付数量',\n" +
-                "  `status` int(11) DEFAULT NULL COMMENT '月账单状态',\n" +
-                "  `created_time` datetime DEFAULT NULL,\n" +
-                "  `create_by` varchar(50) DEFAULT NULL,\n" +
-                "  `updatetime` datetime DEFAULT NULL,\n" +
-                "  `updated_by` varchar(50) DEFAULT NULL,\n" +
-                "  PRIMARY KEY (`id`),\n" +
-                "  KEY `shop_id` (`shop_id`,`bill_year`,`bill_month`)\n" +
-                ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;",sqlContents,ids);
+        Sql2Spring test = new Sql2Spring("sql from sequel Pro",sqlContents,ids);
 
     }
 }
